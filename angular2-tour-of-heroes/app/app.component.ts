e9cdf3083ebbe6293ccd65e82c7514a926044fcd
Zewen Li <zewenli@google.com>
2016-08-06 18:33:09 -0700

    Refactor the master/detail view into separate components.

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -12,13 +14,6 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
-
-
-export class Hero {
-  id: number;
-  name: string;
-}
-
 
 @Component({
   styles: [`
@@ -72,25 +67,19 @@ export class Hero {
   `],
 
   selector: 'my-app',
+  directives: [HeroDetailComponent],
   template: `
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="let hero of heroes"
-      [class.selected]="hero === selectedHero" 
-      (click)="onSelect(hero)">
-      <span class="badge">{{hero.id}}</span> 
-      {{hero.name}}
-    </li>
-  </ul>
-
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero" 
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> 
+        {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `
 })
 export class AppComponent {
