5a769f413f9b10fa54d992d71a0386ffec944ae6
Zewen Li <zewenli@google.com>
2016-08-07 00:33:59 -0700

    Create a reusable service to manage our hero data calls.

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   styles: [`
@@ -68,6 +57,7 @@ const HEROES: Hero[] = [
 
   selector: 'my-app',
   directives: [HeroDetailComponent],
+  providers: [HeroService], 
   template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
@@ -83,7 +73,14 @@ const HEROES: Hero[] = [
   `
 })
 export class AppComponent {
-  public heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+  ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes() {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+  heroes: Hero[];
   selectedHero: Hero;
   title = 'Tour of Heroes';
   hero: Hero = {
